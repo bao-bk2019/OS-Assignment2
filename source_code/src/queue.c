3e02f6ec7a669e78706c680f91eb39278e3d5198
bao-bk2019 <duongquanbao19962012@gmail.com>
2020-07-04 01:07:38 +0700

    queue and dequeue

--- source_code/src/queue.c
+++ source_code/src/queue.c
@@ -8,12 +8,31 @@ int empty(struct queue_t * q) {
 
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
 	/* TODO: put a new process to queue [q] */	
+	if (q->size == MAX_QUEUE_SIZE) return;
+	q->proc[q->size] = proc; //input process
+	q->size++; // increase size of queue 
 }
 
 struct pcb_t * dequeue(struct queue_t * q) {
 	/* TODO: return a pcb whose prioprity is the highest
 	 * in the queue [q] and remember to remove it from q
 	 * */
-	return NULL;
+	if (q->size == 0) return NULL;
+	int i = 0, j;
+	// find the process have a highest priority
+	for (j = 1; j < q->size; j++) {
+		if (q->proc[j]->priority < q->proc[i]->priority) {
+			i = j;
+		}
+	}
+	//assignment the process have a highest priority to "temp" and remove it from q
+	// replace others together and decrease size of q 
+	struct pcb_t * temp = q->proc[i];
+	for (int k = i+1; k < q->size; k++) {
+		q->proc[k-1] = q->proc[k];
+	}
+	q->size--;
+
+	return temp;
 }
 
